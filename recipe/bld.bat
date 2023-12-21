d11257e412c0cb6a671950e5cd4b4876cda32c48
Tobias Fischer <info@tobiasfischer.info>
2023-12-21 15:36:59 +1000

    Delete recipe/bld.bat

--- recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CXX=%%dummy:\=\\%%")
-
-echo %CXX%
-
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CC=%%dummy:\=\\%%")
-
-echo %CC%
-
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
